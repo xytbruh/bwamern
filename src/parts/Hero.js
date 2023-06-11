0ca2bc0fce56db1f6b25c9ae8661384756ab06b8
Dimas Nugraha <127030192+xytbruh@users.noreply.github.com>
2023-06-11 21:47:58 +0700

    finish componen most pick and categories

--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -5,7 +5,7 @@ import IconCities from "assets/images/icons/icon-cities.svg";
 import IconTraveler from "assets/images/icons/icon-traveler.svg";
 import IconTreasure from "assets/images/icons/icon-treasure.svg";
 import Button from "elements/Button";
-
+import formatNumber from "utils/formatNumber";
 export default function Hero(props) {
   function showMostPicked() {
     window.scrollTo({
@@ -13,37 +13,38 @@ export default function Hero(props) {
       behavior: "smooth",
     });
   }
+
   return (
     <section className="container pt-4">
       <div className="row align-items-center">
         <div className="col-auto pr-5" style={{ width: 530 }}>
-          <h1 className="h2 font-weight-bold line-height-1 mb-3">
+          <h1 className="font-weight-bold line-height-1 mb-3">
             Forget Busy Work, <br /> Start Next Vacation
           </h1>
-          <p className="mb-5 font-weight-light text-gray-500 w-75">
+          <p className="mb-4 font-weight-light text-gray-500 w-75" style={{ lineHeight: "170%" }}>
             we provide what you need to enjoy your holiday with family. Time to make another memorable moments
           </p>
           <Button className="btn px-5" hasShadow isPrimary onClick={showMostPicked}>
-            Show Me
+            Show Me Now
           </Button>
 
-          <div className="row mt-5">
+          <div className="row" style={{ marginTop: "80px" }}>
             <div className="col-auto" style={{ marginRight: 35 }}>
               <img width="36" height="36" src={IconTraveler} alt={`${props.data.travelers} Travelers`} />
               <h6 className="mt-3">
-                {props.data.travelers} <span className="text-gray-500 font-weight-light">Travelers</span>
+                {formatNumber(props.data.travelers)} <span className="text-gray-500 font-weight-light">Travelers</span>
               </h6>
             </div>
             <div className="col-auto" style={{ marginRight: 35 }}>
               <img width="36" height="36" src={IconTreasure} alt={`${props.data.treasures} Treasure`} />
               <h6 className="mt-3">
-                {props.data.treasures} <span className="text-gray-500 font-weight-light">Treasure</span>
+                {formatNumber(props.data.treasures)} <span className="text-gray-500 font-weight-light">Treasure</span>
               </h6>
             </div>
             <div className="col-auto" style={{ marginRight: 35 }}>
               <img width="36" height="36" src={IconCities} alt={`${props.data.cities} Cities`} />
               <h6 className="mt-3">
-                {props.data.cities} <span className="text-gray-500 font-weight-light">Cities</span>
+                {formatNumber(props.data.cities)} <span className="text-gray-500 font-weight-light">Cities</span>
               </h6>
             </div>
           </div>
